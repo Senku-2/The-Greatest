2d704b598d3fa2535dfa6c914d57bf63a0b34c32
Senku-2 <sapiensjc@gmail.com>
2024-08-27 07:37:57 +0100

    Update 🇨🇩.js

--- scripts/cmds/🇲🇬.js
+++ scripts/cmds/🇲🇬.js
@@ -1,19 +1,19 @@
 module.exports = {
  config: {
- name: "🇲🇬",
+ name: "🇨🇩",
  version: "1.0",
  author: "thea",
  countDown: 5,
  role: 0,
- shortDescription: "Mada flag🇲🇬🇲🇬💬",
+ shortDescription: "Mada flag🇨🇩 🇨🇩",
  longDescription: "no prefix",
  category: "no prefix",
  }, 
  onStart: async function(){}, 
  onChat: async function({ event, message, getLang }) {
- if (event.body && event.body.toLowerCase() === "🇲🇬") {
+ if (event.body && event.body.toLowerCase() === "🇨🇩") {
  return message.reply({
- body: "「🇲🇬」- Anjara/Aesther",
+ body: "「🇨🇩」- Anjara/Aesther",
  attachment: await global.utils.getStreamFromURL("https://i.ibb.co/7GVF1DD/image.jpg")
  });
  }
